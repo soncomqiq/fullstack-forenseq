3e80b2bcb40bbfa52767b1fdac6fbd1f54261b40
soncomqiq <sonter@buzzfreeze.com>
2020-02-04 15:36:58 +0700

    fixed bugs

--- front-end-forensic/src/NavBar/GuestBar.js
+++ front-end-forensic/src/NavBar/GuestBar.js
@@ -5,42 +5,42 @@ const MenuItemGroup = Menu.ItemGroup;
 
 export default class GuestNavbar extends Component {
 
-    handleMenuClick = ({ key }) => {
-        console.log(key)
-            {localStorage.setItem('currentMenu',key)}
-    }
+  handleMenuClick = ({ key }) => {
+    console.log(key)
+    { localStorage.setItem('currentMenu', key) }
+  }
 
-    render() {
-        const colorTheme = '';
-        return (
-            <Menu
-                onClick={this.handleMenuClick}
-                selectedKeys={[localStorage.getItem('currentMenu')]}
-                mode="horizontal"
-                theme="light"
-                style={{ lineHeight: '64px', backgroundColor:colorTheme }}
-            >
-                <Menu.Item key="home">
-                    <a href="/" ><Icon type="home" />Home</a>
-                </Menu.Item>
-                <Menu.Item key="search">
-                    <a href="/search" ><Icon type="search" />Search</a>
-                </Menu.Item>
-                <SubMenu key="stats" title={<span><Icon type="radar-chart" />Statistics</span>}>
-                    <MenuItemGroup title="Group By Locus" style={{backgroundColor:colorTheme}}>
-                        <Menu.Item key="setting:1"><a href="/stats/graph" ><Icon type="bar-chart" />Graph</a></Menu.Item>
-                        <Menu.Item key="setting:2"><a href="/stats/map" ><Icon type="google" />Map</a></Menu.Item>
-                    </MenuItemGroup>
-                </SubMenu>
-                <SubMenu title={<span><Icon type="line-chart" />Analysis</span>}>
-                    <MenuItemGroup title="Analysis" style={{backgroundColor:colorTheme}}>
-                        <Menu.Item key="graph"><a href="/analysis/kinship" ><Icon type="team" />Kinchip Analysis</a></Menu.Item>
-                    </MenuItemGroup>
-                </SubMenu>
-                <Menu.Item key="login" style={{ float: 'right' }}>
-                    <Button type="dashed"><a href="/login"><Icon type="login" />Login</a></Button>
-                </Menu.Item>
-            </Menu>
-        )
-    }
+  render() {
+    const colorTheme = '';
+    return (
+      <Menu
+        onClick={this.handleMenuClick}
+        selectedKeys={[localStorage.getItem('currentMenu')]}
+        mode="horizontal"
+        theme="light"
+        style={{ lineHeight: '64px', backgroundColor: colorTheme }}
+      >
+        <Menu.Item key="home">
+          <a href="/" ><Icon type="home" />Home</a>
+        </Menu.Item>
+        <Menu.Item key="search">
+          <a href="/search" ><Icon type="search" />Search</a>
+        </Menu.Item>
+        <SubMenu key="stats" title={<span><Icon type="radar-chart" />Statistics</span>}>
+          <MenuItemGroup title="Group By Locus" style={{ backgroundColor: colorTheme }}>
+            <Menu.Item key="graph"><a href="/stats/graph" ><Icon type="bar-chart" />Graph</a></Menu.Item>
+            <Menu.Item key="map"><a href="/stats/map" ><Icon type="google" />Map</a></Menu.Item>
+          </MenuItemGroup>
+        </SubMenu>
+        <SubMenu title={<span><Icon type="line-chart" />Analysis</span>}>
+          <MenuItemGroup title="Analysis" style={{ backgroundColor: colorTheme }}>
+            <Menu.Item key="kinship"><a href="/analysis/kinship" ><Icon type="team" />Kinchip Analysis</a></Menu.Item>
+          </MenuItemGroup>
+        </SubMenu>
+        <Menu.Item key="login" style={{ float: 'right' }}>
+          <Button type="dashed"><a href="/login"><Icon type="login" />Login</a></Button>
+        </Menu.Item>
+      </Menu>
+    )
+  }
 }
--- front-end-forensic/src/Page/PageKinshipAnalysis.js
+++ front-end-forensic/src/Page/PageKinshipAnalysis.js
@@ -5,43 +5,47 @@ import { Modal } from 'antd';
 import Example from '../Container/Example';
 
 const DescriptionBody = (
-    <div>
-        Upload file to calculate probability.
+  <div>
+    Upload file to calculate probability.
     </div>
 );
 
 class Body extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            visible: false,
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
     }
+  }
 
-    info() {
-        Modal.info({
-            title: 'Example For Kinship',
-            content: (
-                <Example/>
-            ),
-            onOk() { },
-        });
-    }
+  componentWillMount() {
+    localStorage.setItem('currentMenu', 'kinship')
+  }
 
-    render() {
-        const ButtonComponent = (
-            <div>
-                <a onClick={this.info}>Click here for example file</a>
-            </div>
-        );
-        const visible = this.state.visible;
-        return (
-            <div className="Body">
-                <Box title="Description" body={DescriptionBody} btnComponent={ButtonComponent} link="/example" />
-                <FileUploadKinship />
-            </div>
-        );
-    }
+  info() {
+    Modal.info({
+      title: 'Example For Kinship',
+      content: (
+        <Example />
+      ),
+      onOk() { },
+    });
+  }
+
+  render() {
+    const ButtonComponent = (
+      <div>
+        <a onClick={this.info}>Click here for example file</a>
+      </div>
+    );
+    const visible = this.state.visible;
+    return (
+      <div className="Body">
+        <Box title="Description" body={DescriptionBody} btnComponent={ButtonComponent} link="/example" />
+        <FileUploadKinship />
+      </div>
+    );
+  }
 }
 
 export default Body
