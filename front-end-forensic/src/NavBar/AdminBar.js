db8bc3c0b505c768e606d7e30b81efe485b5e4da
soncomqiq <sonter@buzzfreeze.com>
2020-02-12 16:33:50 +0700

    edit notification

--- front-end-forensic/src/NavBar/AdminBar.js
+++ front-end-forensic/src/NavBar/AdminBar.js
@@ -6,66 +6,70 @@ const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
 export default class AdminNavbar extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            isLoginActive: false,
-        }
+    this.state = {
+      isLoginActive: false,
     }
+  }
 
-    handleMenuClick = ({ key }) => {
-        console.log(key)
-        if (key === "logout") {
-            this.props.onLogout();
-        } else {
-            { localStorage.setItem('currentMenu', key) }
-        }
+  handleMenuClick = ({ key }) => {
+    console.log(key)
+    if (key === "logout") {
+      this.props.onLogout();
+    } else {
+      { localStorage.setItem('currentMenu', key) }
     }
+  }
 
-    render() {
-        if (this.props.isLoading) {
-            return <LoadingIndicator />;
-        }
-        const colorTheme = '';
-        console.log(localStorage.getItem('currentMenu'))
-        return (
-            <Menu
-                onClick={this.handleMenuClick}
-                selectedKeys={[localStorage.getItem('currentMenu')]}
-                mode="horizontal"
-                theme="light"
-                style={{ lineHeight: '64px', backgroundColor: colorTheme }}
-            >
-                <Menu.Item key="home">
-                    <a href="/" ><Icon type="home" />Home</a>
-                </Menu.Item>
-                <Menu.Item key="search">
-                    <a href="/search" ><Icon type="search" />Search</a>
-                </Menu.Item>
-                <SubMenu key="stats" title={<span><Icon type="radar-chart" />Statistics</span>}>
-                    <MenuItemGroup title="Group By Locus" style={{ backgroundColor: colorTheme }}>
-                        <Menu.Item key="graph"><a href="/stats/graph" ><Icon type="bar-chart" />Graph</a></Menu.Item>
-                        <Menu.Item key="map"><a href="/stats/map" ><Icon type="google" />Map</a></Menu.Item>
-                    </MenuItemGroup>
-                </SubMenu>
-                <SubMenu key="analysis" title={<span><Icon type="line-chart" />Analysis</span>}>
-                    <MenuItemGroup title="Analysis" style={{ backgroundColor: colorTheme }}>
-                        <Menu.Item key="kinship"><a href="/analysis/kinship" ><Icon type="team" />Kinchip Analysis</a></Menu.Item>
-                    </MenuItemGroup>
-                </SubMenu>
-                <SubMenu key="menu" style={{ float: 'right' }} title={<span><Icon type="setting" />Lab User Menu</span>}>
-                    <MenuItemGroup title="Function" style={{ backgroundColor: colorTheme }}>
-                        <Menu.Item key="add"><a href="/adddata" ><Icon type="file-add" />Add data</a></Menu.Item>
-                        <Menu.Item key="addperson"><a href="/uploadpersons" ><Icon type="usergroup-add" />Add Persons</a></Menu.Item>
-                        <Menu.Item key="cedata"><a href="/uploadcedata" ><Icon type="file-text" />Add CE Data</a></Menu.Item>
-                        <Menu.Item key='listperson'><a href="/listpersons" ><Icon type="team" />Person List</a></Menu.Item>
-                        <Menu.Item key="isnp"><a href="/isnpstat" ><Icon type="box-plot" />iSNPs</a></Menu.Item>
-                        <Menu.Item key="alignment"><a href="/seqalign" ><Icon type="barcode" />Seq Alignment</a></Menu.Item>
-                        <Menu.Item key="logout"><Icon type="logout" />Logout</Menu.Item>
-                    </MenuItemGroup>
-                </SubMenu>
-            </Menu>
-        )
+  render() {
+    if (this.props.isLoading) {
+      return <LoadingIndicator />;
     }
+    const colorTheme = '';
+    console.log(localStorage.getItem('currentMenu'))
+    return (
+      <Menu
+        onClick={this.handleMenuClick}
+        selectedKeys={[localStorage.getItem('currentMenu')]}
+        mode="horizontal"
+        theme="light"
+        style={{ lineHeight: '64px', backgroundColor: colorTheme }}
+      >
+        <Menu.Item key="home">
+          <a href="/" ><Icon type="home" />Home</a>
+        </Menu.Item>
+        <Menu.Item key="search">
+          <a href="/search" ><Icon type="search" />Search</a>
+        </Menu.Item>
+        <SubMenu key="stats" title={<span><Icon type="radar-chart" />Statistics</span>}>
+          <MenuItemGroup title="Group By Locus" style={{ backgroundColor: colorTheme }}>
+            <Menu.Item key="graph"><a href="/stats/graph" ><Icon type="bar-chart" />Graph</a></Menu.Item>
+            <Menu.Item key="map"><a href="/stats/map" ><Icon type="google" />Map</a></Menu.Item>
+          </MenuItemGroup>
+        </SubMenu>
+        <SubMenu key="analysis" title={<span><Icon type="line-chart" />Analysis</span>}>
+          <MenuItemGroup title="Analysis" style={{ backgroundColor: colorTheme }}>
+            <Menu.Item key="kinship"><a href="/analysis/kinship" ><Icon type="team" />Kinchip Analysis</a></Menu.Item>
+          </MenuItemGroup>
+        </SubMenu>
+        <SubMenu key="menu" style={{ float: 'right' }} title={<span><Icon type="setting" />Lab User Menu</span>}>
+          <MenuItemGroup title="Manange sample" style={{ backgroundColor: colorTheme }}>
+            <Menu.Item key="addperson"><a href="/uploadpersons" ><Icon type="usergroup-add" />Add persons</a></Menu.Item>
+            <Menu.Item key='listperson'><a href="/listpersons" ><Icon type="team" />Browse persons</a></Menu.Item>
+          </MenuItemGroup>
+          <MenuItemGroup title="Manage STR data" style={{ backgroundColor: colorTheme }}>
+            <Menu.Item key="add"><a href="/adddata" ><Icon type="file-add" />Add ForenSeq</a></Menu.Item>
+            <Menu.Item key="cedata"><a href="/uploadcedata" ><Icon type="file-text" />Add CE data</a></Menu.Item>
+          </MenuItemGroup>
+          <MenuItemGroup title="Analysis" style={{ backgroundColor: colorTheme }}>
+            <Menu.Item key="isnp"><a href="/isnpstat" ><Icon type="box-plot" />iSNPs</a></Menu.Item>
+            <Menu.Item key="alignment"><a href="/seqalign" ><Icon type="barcode" />Seq Alignment</a></Menu.Item>
+          </MenuItemGroup>
+          <Menu.Item key="logout"><Icon type="logout" />Logout</Menu.Item>
+        </SubMenu>
+      </Menu >
+    )
+  }
 }
